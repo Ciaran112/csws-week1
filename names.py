4f22c46302f37f569fc8d6df12cdbe72726e4e11
Ciaran <clunt2002@gmail.com>
2022-01-25 14:01:16 +0000

    3-1 and 3-2

---
+++ names.py
@@ -0,0 +1,11 @@
+names=['John','Sam','Jay','Cam']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+print(f"My friend {names[0]} is very tall")
+print(f"My Frined {names[1]} is extremely smart")
+print(f"My friend {names[2]} is an alcaholic")
+print(f"My friend {names[3]} is very mysterious")
+
