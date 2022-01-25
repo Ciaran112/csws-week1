fa7b2641469170037edd701d86417a3aef7ded03
Ciaran <clunt2002@gmail.com>
2022-01-25 14:20:23 +0000

    task 3 - 8

---
+++ seeing_the_world.py
@@ -0,0 +1,32 @@
+places=["USA","Japan","Greece","Germany","Brazil"]
+
+print("Original Order")
+print(places)
+
+print("Sorted Order")
+print(sorted(places))
+
+print("Original Order")
+print(places)
+
+print("Sorted Reverse Order")
+print(sorted(places, reverse=True))
+
+print("Original Order")
+print(places)
+
+print("Reverse order")
+places.reverse()
+print(places)
+
+print("Original Order")
+places.reverse()
+print(places)
+
+print("Alphabetical Order")
+places.sort()
+print(places)
+
+print("Reverse Alphabetical order")
+places.sort(reverse=True)
+print(places)
